00bcce44f843e398b0ec646d98388f3ed3f4a3b1
shocker <fjana@gmail.com>
2025-02-15 20:28:25 -0300

    added pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,20 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [
+    tailwindcss(),
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        theme_color: "#000000",
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
